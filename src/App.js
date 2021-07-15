a78739395e8f62f0441852f18a3b718f27c4ab77
Oleg <oleg.mykytchuk@gmail.com>
2021-07-15 15:21:16 +0300

    Drum

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {bankOne, bankTwo} from "./component/state";
+import React, { useState} from "react";
+import Drum from "./component/Drum";
+import 'antd/dist/antd.css';
+import Pad from "./component/Pad";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [value, setValue] = useState('')
+    const [isTrue, setIsTrue] = useState(false)
+    const [volume, setVolume] = useState(0.5)
+    const [isPower,setIsPower] = useState(true)
+
+    let [...arrayBank] =  isTrue ? [...bankTwo] : [...bankOne];
+
+    return (
+        <div id='drum-machine' >
+            <div id='display' className="main">
+                {
+                     arrayBank.map(a => {return (
+                            <Drum
+                                key={a.id}
+                                a={a}
+                                volume={volume}
+                                setValue={setValue}
+                                isPower={isPower}
+                            />)})
+                }
+            </div>
+            <Pad values={value}
+                 setVolume={setVolume}
+                 setIsTrue={setIsTrue}
+                 setIsPower={setIsPower}
+                 volume={volume}
+            isPower={isPower}/>
+        </div>
+    )
 }
 
 export default App;
